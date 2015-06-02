c28ec56827c546f2ba67353be0b1d9cb73bc5f2e
niwatolli3 <niwatolli3@gmail.com>
2015-06-02 21:32:24 +0900

    modified qemu.bat: added variable QEMU_DIR

--- bootloader/qemu.bat
+++ bootloader/qemu.bat
@@ -1,1 +1,3 @@
-V:\Program\qemu-0.9.0-windows\qemu-system-x86_64.exe -L V:\Program\qemu-0.9.0-windows -m 128 -fda floppy.img -soundhw all -localtime
+SET QEMU_DIR="V:\Program\qemu-0.9.0-windows"
+
+%QEMU_DIR%\qemu-system-x86_64.exe -L %QEMU_DIR% -m 128 -fda floppy.img -soundhw all -localtime
